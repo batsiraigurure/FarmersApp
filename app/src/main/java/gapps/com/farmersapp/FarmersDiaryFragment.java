8281d64775fa09392f45ceed3a59e9557e07dfc6
Batsirai Gurure <batsiraigurure@yahoo.com>
2017-05-23 15:31:30 +0200

    Added correct navigation menu items, resource files and relevant fragments

---
+++ app/src/main/java/gapps/com/farmersapp/FarmersDiaryFragment.java
@@ -0,0 +1,20 @@
+package gapps.com.farmersapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FarmersDiaryFragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.farmers_diary_layout, container, false);
+        return myView;
+    }
+}
