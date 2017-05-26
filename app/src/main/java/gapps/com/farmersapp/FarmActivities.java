ef1f95fb36f554f7f2b93dab20d7ad0e676eaebd
Batsirai Gurure <batsiraigurure@yahoo.com>
2017-05-26 16:54:53 +0200

    Edited layout for farmers_diary_layout, added String arrays for tasks to FarmActivities

--- app/src/main/java/gapps/com/farmersapp/FarmActivities.java
+++ app/src/main/java/gapps/com/farmersapp/FarmActivities.java
@@ -1,5 +1,6 @@
 package gapps.com.farmersapp;
 
 public class FarmActivities {
-    public String[] farmingSteps = new String[] {"Test soil PH", "Purchase inputs", "Initial tilling of land", "Seed planting", "Apply organic fertiliser", "Transplanting", "Apply inorganic fertiliser"};
+    public String[] stage1 = new String[] {"Test soil PH", "Purchase inputs", "Initial tilling of land", "Seed planting"};
+    public String[] stage2 = new String[] {"Apply organic fertiliser", "Transplanting", "Apply inorganic fertiliser"};
 }
--- app/src/main/java/gapps/com/farmersapp/FarmersDiaryFragment.java
+++ app/src/main/java/gapps/com/farmersapp/FarmersDiaryFragment.java
@@ -6,10 +6,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class FarmersDiaryFragment extends Fragment{
 
     View myView;
+    Button generateDiary;
+    EditText day;
+    TextView completedTasks;
 
     public int dayInCycle;
 
@@ -17,10 +26,31 @@ public class FarmersDiaryFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.farmers_diary_layout, container, false);
+        day = (EditText) myView.findViewById(R.id.dayEditText);
+        generateDiary = (Button) myView.findViewById(R.id.generateDiaryButton);
+        completedTasks = (TextView) myView.findViewById(R.id.completedTasks);
 
-        /* convert what is in edittext to integer
-        dayInCycle = (myView.findViewById(R.id.dayEditText).toString());
-        */
+        generateDiary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FarmActivities farmActivities = new FarmActivities();
+
+                dayInCycle = Integer.parseInt(day.getText().toString());
+
+                if(dayInCycle<20){
+                    //Display stage1 and stage2 as To Do
+                    completedTasks.setText("");
+                }
+                else if(dayInCycle>=20 && dayInCycle<50){
+                    completedTasks.setText(Arrays.toString(farmActivities.stage1));
+                    //display stage1 as completed and stage2 as To do
+                }
+                else if(dayInCycle>=50){
+                    //Display stage1 and stage2 as completed
+                }
+            }
+        });
 
         return myView;
     }
